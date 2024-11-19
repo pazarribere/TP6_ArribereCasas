e3efad9cb6d18e9e88b71af5c99132266c2795d7
Paz Arribere <marribere@itba.edu.ar>
2024-11-19 11:54:40 -0300

    cambios pero sin probar nada

--- base.py
+++ base.py
@@ -4,7 +4,7 @@ from patient import create_patient_resource
 
 # Enviar el recurso FHIR al servidor HAPI FHIR
 def send_resource_to_hapi_fhir(resource,resource_type):
-    url = f"http://hapi.fhir.org/baseR4/{resource_type}"
+    url = f"http://hapi.fhir.org/baseR5/{resource_type}"
     headers = {"Content-Type": "application/fhir+json"}
     resource_json = resource.json()
 
@@ -22,7 +22,7 @@ def send_resource_to_hapi_fhir(resource,resource_type):
 
 # Buscar el recurso por ID 
 def get_resource_from_hapi_fhir(resource_id, resource_type):
-    url = f"http://hapi.fhir.org/baseR4/{resource_type}/{resource_id}"
+    url = f"http://hapi.fhir.org/baseR5/{resource_type}/{resource_id}"
     response = requests.get(url, headers={"Accept": "application/fhir+json"})
 
     if response.status_code == 200:
@@ -30,5 +30,28 @@ def get_resource_from_hapi_fhir(resource_id, resource_type):
         print(resource)
     else:
         print(f"Error al obtener el recurso: {response.status_code}")
+        print(response.json())
+
+# Buscar el recurso por DNI 
+def get_patient_by_dni(dni):
+    base_url = "http://hapi.fhir.org/baseR5/Patient"
+    
+    # Realizar la búsqueda por identifier
+    query_params = {"identifier": f"http://example.org/fhir/identifier/dni|{dni}"}
+    
+    # Realizar la solicitud GET
+    response = requests.get(base_url, params=query_params, headers={"Accept": "application/fhir+json"})
+    
+    # Verificar el estado de la respuesta
+    if response.status_code == 200:
+        patients = response.json()
+        if 'entry' in patients:
+            print(f"Se encontraron {len(patients['entry'])} paciente(s) con el DNI proporcionado.")
+            for entry in patients['entry']:
+                print(entry['resource'])  # Imprimir los recursos encontrados
+        else:
+            print("No se encontró ningún paciente con el DNI proporcionado.")
+    else:
+        print(f"Error al buscar el paciente: {response.status_code}")
         print(response.json())
 
--- workflow.py
+++ workflow.py
@@ -1,18 +1,33 @@
 from patient import create_patient_resource
-from base import send_resource_to_hapi_fhir, get_resource_from_hapi_fhir
+from condition import create_condition_resource
+from base import send_resource_to_hapi_fhir, get_resource_from_hapi_fhir, get_patient_by_dni
 
 if __name__ == "__main__":
     # Parámetros del paciente (se puede dejar algunos vacíos)
-    family_name = "Doe"
-    given_name = "John"
-    birth_date = "1990-01-01"
+    family_name = "Barry"
+    given_name = "Ben"
+    birth_date = "1999-08-08"
     gender = "male"
-    phone = None 
+    phone = None
+    dni = "41846513"
 
     # Crear y enviar el recurso de paciente
-    patient = create_patient_resource(family_name, given_name, birth_date, gender, phone)
+    patient = create_patient_resource(family_name, given_name, birth_date, gender, phone, dni)
     patient_id = send_resource_to_hapi_fhir(patient, 'Patient')
 
     # Ver el recurso de paciente creado
     if patient_id:
         get_resource_from_hapi_fhir(patient_id,'Patient')
+    
+    get_patient_by_dni(dni)
+
+    condition_resource = create_condition_resource(
+        condition_code="Diabetes tipo 2",
+        patient_id="12345",
+        condition_status="active",
+        onset_date="2020-01-01",  # Usar formato string adecuado
+        end_date="2025-01-01",    # Usar formato string adecuado
+        condition_id="condition123"
+    )
+    
+    print(condition_resource.json())
